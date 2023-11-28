fde904dedff2fa7c03d1bf4b68cf559dcdbcfe95
rwormsbnecher <rodney22@live.nl>
2023-11-28 23:49:19 +0100

    clean up fetch call

--- src/store/cities/fetchCities.ts
+++ src/store/cities/fetchCities.ts
@@ -4,17 +4,15 @@ export interface CitiesApiResponse {
 	cities: City[];
 }
 
-// Async function to fetch cities
 export const fetchCities = async (): Promise<CitiesApiResponse> => {
 	try {
 		const response = await fetch("https://greensocapi.azurewebsites.net/api/Cities");
 		if (!response.ok) {
-			throw new Error("Network response was not ok");
+			throw new Error("Network error");
 		}
 		const data = (await response.json()) as CitiesApiResponse;
 		return data;
 	} catch (error) {
-		// Handle or throw the error depending on your use case
 		throw error;
 	}
 };
